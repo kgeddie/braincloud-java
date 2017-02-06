eccb750b94414962199904069bba6e4157837647
Brad Hill <bradleyh@bitheads.com>
2017-02-06 12:40:08 -0500

    Version to 3.2.1

--- TestBCClient/brainCloud/src/main/java/com/bitheads/braincloud/client/BrainCloudClient.java
+++ TestBCClient/brainCloud/src/main/java/com/bitheads/braincloud/client/BrainCloudClient.java
@@ -45,7 +45,7 @@ public class BrainCloudClient {
     private String _languageCode;
     private double _timeZoneOffset;
 
-    private final static String BRAINCLOUD_VERSION = "3.3.0";
+    private final static String BRAINCLOUD_VERSION = "3.2.1";
 
     private BrainCloudRestClient _restClient;
 
